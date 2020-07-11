252656ba499e095b404ed4fd3bcf647d634412ae
sinji imagawa <yui.ibu.rami@gmail.com>
2020-07-11 18:11:54 +0900

    新規投稿機能 new, create を実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,17 @@
 class MessagesController < ApplicationController
+  def new
+    @message = Message.new
+  end
+
+  def create
+    Message.create(message_params)
+  end
+
+  private
+
+  # 何でもかんでも登録出来ないようにする 他では使わないのでプライベートにする
+
+  def message_params
+    params.require(:message).permit(:title, :content)
+  end
 end
