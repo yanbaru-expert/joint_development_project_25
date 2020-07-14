d0c883366fbe6640a021bd0fc271cca3a7ec5ea9
ito46010 <g.itoitou@gmail.com>
2020-07-14 18:18:01 +0900

    新規投稿機能new,createを実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,16 @@
 class TasksController < ApplicationController
+　　def new
+    @task = Task.new
+   end
+
+   def create
+    Task.create(task_params)
+   end
+
+   private
+   def task_params
+    params.require(:task).permit(:title, :content)
+   end
+   
+
 end
